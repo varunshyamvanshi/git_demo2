9786985d6cc14e8247f86f3442a0527468e5435d
varunshyamvanshi <varunshyamvanshi@gmail.com>
2025-01-25 00:49:06 +0530

    updated prc2.py

--- prc2.py
+++ prc2.py
@@ -1,9 +1,12 @@
 def decorator_func(original_func):
   def wrapper():
     print("hey it's decorated now")
-    cities()
-  return wrapper()  
+    original_func()
+  return wrapper
 
 @decorator_func
 def cities():
   print("panipat,karnal,kurukshetra")
+
+
+cities()
